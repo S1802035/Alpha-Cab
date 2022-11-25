2b05153d80483620e418d330cfb0e846f1a1a39e
S1802035 <s1802035@students.villacollege.edu.mv>
2022-11-25 18:49:57 +0500

    Packages import

--- src/java/Customer.java
+++ src/java/Customer.java
@@ -9,6 +9,15 @@ Description: -
 Note: Uncomment codes to execute and comment them when not in use.
  */
 
+// Importing packages.
+import java.io.*;
+import java.sql.*; // PreparedStatement, ResultSet, SQLException
+import java.util.logging.*; // Level, Logger
+import javax.swing.*; // Calling pop up message library
+import javax.servlet.*;
+import javax.servlet.annotation.*; // WebServlet
+import javax.servlet.http.*; // Servelet, ServletRequest, ServletResponse
+
 // Main class.
 public class Customer {
     // Main function.
