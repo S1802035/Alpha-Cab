9c35d63f2a94b9f60027a310aa905ef8acd1298b
S1802035 <s1802035@students.villacollege.edu.mv>
2022-11-25 18:40:41 +0500

    New file created

---
+++ src/java/Customer.java
@@ -0,0 +1,19 @@
+/*
+Names: Adham Yoonus, Ahmed Affaan, Ammar Abdul Rahman, Aminath Muzuna
+Title: Customer.java
+Folder: Alpha-Cab
+Date: 25/11/2022
+Country: Republic of Maldives
+Code version: -
+Description: -
+Note: Uncomment codes to execute and comment them when not in use.
+ */
+
+// Main class.
+public class Customer {
+    // Main function.
+    public static void main(String[] args) {
+        // Placeholder message.
+        System.out.println("Customer Panel");
+    }
+}
